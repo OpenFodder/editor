062a90c5ee6076c28fa725afbb220716092d6662
Robert Crossfield <robcrossfield@gmail.com>
2017-11-05 06:48:26 +1100

    Add the sprite toolbox

---
+++ Source/Dialogs/ToolboxSprites.cpp
@@ -0,0 +1,132 @@
+#include "stdafx_ofed.hpp"
+#include "ofed.hpp"
+#include "ui_ToolboxSprites.h"
+
+#include <qpixmap.h>
+#include <qpainter.h>
+#include <qevent.h>
+
+cToolboxSprites::cToolboxSprites(QWidget *parent, Qt::WindowFlags f) : QDialog(parent, f) {
+
+	mSpriteSurface = new cSurface(20 * 17, 9 * 17);
+
+	mUi = new Ui_ToolboxSprites();
+	mUi->setupUi(this);
+
+	RenderSprites();
+}
+
+cToolboxSprites::~cToolboxSprites() {
+
+	delete mSpriteSurface;
+}
+
+void cToolboxSprites::RenderSprites() {
+	size_t width = this->width();
+	size_t height = this->height();
+
+	size_t X = 0, Y = 0;
+	size_t BigY = 0;
+	size_t MultiplierX = g_Fodder.mVersion->mPlatform == ePlatform::Amiga ? 8 : 16;
+	size_t MultiplierY = g_Fodder.mVersion->mPlatform == ePlatform::Amiga ? 2 : 16;
+
+	mSpriteRanges.clear();
+
+	g_Graphics.SetActiveSpriteSheet(eSPRITE_IN_GAME);
+
+	for (size_t SpriteID = 0; SpriteID < 111; ++SpriteID) {
+		sSprite Sprite;
+
+		int32 AnimID = g_SpriteAnim[SpriteID];
+		if (AnimID < 0)
+			continue;
+
+		size_t SpriteWidth = g_Fodder.mSpriteSheetPtr[AnimID][0].mColCount * MultiplierX;
+		size_t SpriteHeight = g_Fodder.mSpriteSheetPtr[AnimID][0].mRowCount * MultiplierY;
+		
+		size_t SpriteWidth2 = g_Fodder.mSpriteSheetPtr[AnimID][0].mColCount;
+		size_t SpriteHeight2 = g_Fodder.mSpriteSheetPtr[AnimID][0].mRowCount;
+
+		Sprite.field_0 = -0x40;
+		Sprite.field_4 = 9;
+		Sprite.field_52 = 0;
+		Sprite.field_20 = 0;
+
+		cSurface *Surface = new cSurface(SpriteWidth, SpriteHeight);
+
+		g_Graphics.PaletteSet(Surface);
+		g_Fodder.Sprite_Draw_Frame(&Sprite, AnimID, 0, Surface);
+
+		if (X + SpriteWidth > (20 * 17)) {
+			X = 0;
+			Y += BigY;
+			BigY = 0;
+		}
+
+		mSpriteRanges.push_back(sRange(X, Y, SpriteWidth, SpriteHeight, SpriteID));
+
+		SDL_Rect SrcRect;
+		SrcRect.x = 0;
+		SrcRect.y = 0;
+		SrcRect.w = SpriteWidth;
+		SrcRect.h = SpriteHeight;
+
+		SDL_Rect DstRect;
+		DstRect.x = X;
+		DstRect.y = Y;
+		DstRect.w = SpriteWidth;
+		DstRect.h = SpriteHeight;
+
+		g_Graphics.PaletteSet(Surface);
+		Surface->surfaceSetToPaletteNew();
+		Surface->draw();
+
+		SDL_BlitSurface(Surface->GetSurface(), &SrcRect, mSpriteSurface->GetSurface(), &DstRect);
+
+		if (SpriteHeight > BigY)
+			BigY = SpriteHeight;
+
+		X += SpriteWidth;
+		if (X >= (20 * 17)) {
+			X = 0;
+			Y += BigY;
+			BigY = 0;
+		}
+
+		delete Surface; 
+	}
+
+	// Load the tileset palette to the surface
+
+	// Copy the image into a QImage
+	SDL_Surface* Source = mSpriteSurface->GetSurface();
+	mImage = QImage(static_cast<uchar*>(Source->pixels), Source->w, Source->h, QImage::Format_RGB32);
+
+	mScaleWidth = (static_cast<double>(size().width()) / static_cast<double>(mImage.width()));
+	mScaleHeight = (static_cast<double>(size().height()) / static_cast<double>(mImage.height()));
+}
+
+void cToolboxSprites::paintEvent(QPaintEvent* e) {
+
+	QPainter painter(this);
+	QRectF Dest(0, 0, size().width(), size().height());
+	QRectF Src(0, 0, mImage.width(), mImage.height());
+
+	painter.drawImage(Dest, mImage, Src);
+}
+
+void cToolboxSprites::mousePressEvent(QMouseEvent *eventPress) {
+	size_t MouseX = eventPress->x() / mScaleWidth;
+	size_t MouseY = eventPress->y() / mScaleHeight;
+
+	uint32 TileX = MouseX / 18;
+	uint32 TileY = MouseY / 18;
+
+	// 20 Tiles per row
+	uint32 TileID = (20 * TileY) + TileX;
+
+	if (eventPress->button() == Qt::MouseButton::LeftButton) {
+
+		g_OFED->SetCursorTileID(TileID);
+	}
+}
---
+++ Source/Dialogs/ToolboxSprites.hpp
@@ -0,0 +1,38 @@
+class Ui_ToolboxSprites;
+
+struct sRange {
+	size_t mX, mY;
+	size_t mWidth, mHeight;
+	size_t mSpriteID;
+
+	sRange(size_t pX, size_t pY, size_t pWidth, size_t pHeight, size_t pSpriteID) {
+		mX = pX;
+		mY = pY;
+		mWidth = pWidth;
+		mHeight = pHeight;
+		mSpriteID = pSpriteID;
+	}
+};
+
+class cToolboxSprites : public QDialog {
+	Ui_ToolboxSprites *mUi;
+
+	std::vector< sRange >	 mSpriteRanges;
+	cSurface				*mSpriteSurface;
+
+	QImage			 mImage;
+
+	double			 mScaleWidth, mScaleHeight;
+
+protected:
+
+	void paintEvent(QPaintEvent* e);
+	void mousePressEvent(QMouseEvent *eventPress);
+
+public:
+	cToolboxSprites(QWidget *parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+	~cToolboxSprites();
+
+	void RenderSprites();
+
+};
